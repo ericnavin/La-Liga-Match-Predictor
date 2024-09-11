8dffda18c5297ff19d547eb4acbd1af89ee1024f
Eric <142185038+ericnavin@users.noreply.github.com>
2024-09-11 15:41:13 -0400

    Update La-Liga-Predictor.py

--- La-Liga-Predictor.py
+++ La-Liga-Predictor.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
 
+# Load the data
+laliga_data = pd.read_csv("laliga2020.csv", encoding='ISO-8859-1')
